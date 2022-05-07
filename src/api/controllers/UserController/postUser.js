e28dea3c0da1efb6d092f390487b3288497d9cde
Martin Barnes <martinbarnes.dev@gmail.com>
2022-05-07 17:37:11 +0100

    Password hashing.

--- src/api/controllers/UserController/postUser.js
+++ src/api/controllers/UserController/postUser.js
@@ -1,18 +1,30 @@
 const { User } = require('../../models');
+const bcrypt = require('bcrypt');
 
 const postUser = async (req, res) => {
 	const { username, password } = req.body;
 
-	await User.create({
-		username: username,
-		password: password,
-	})
-		.then(() => {
-			res.status(200).json({ action: 'Created user.' });
+	await bcrypt
+		.hash(password, 10)
+		.then((hash) => {
+			User.create({
+				username: username,
+				password: hash,
+			})
+				.then(() => {
+					res.status(200).json({
+						success: 'true',
+						username: username,
+						password: hash,
+					});
+				})
+				.catch((err) => {
+					console.error(err);
+					res.status(404).json({ action: 'Could not create user.' });
+				});
 		})
 		.catch((err) => {
 			console.error(err);
-			res.status(404).json({ action: 'Could not create user.' });
 		});
 };
 
