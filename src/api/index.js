82a7bc3218b4ea5ea14a482070c5a69c372f3970
Martin Barnes <martinbarnes.dev@gmail.com>
2022-05-07 16:56:07 +0100

    Last initial.

--- src/api/index.js
+++ src/api/index.js
@@ -14,7 +14,7 @@ db.sequelize
 	.sync()
 	.then(() => {
 		app.listen(PORT, () => {
-			console.log(`SERVER LIVE @ http://localhost:${PORT}/`);
+			console.log(`http://localhost:${PORT}/`);
 		});
 	})
 	.catch((err) => {
