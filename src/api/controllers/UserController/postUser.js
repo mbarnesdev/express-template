059048e74be260445b6e57f311990ece8abb3558
Martin Barnes <martinbarnes.dev@gmail.com>
2022-05-07 17:19:32 +0100

    User insert.

--- src/api/controllers/UserController/postUser.js
+++ src/api/controllers/UserController/postUser.js
@@ -1,5 +1,19 @@
-const postUser = (req, res) => {
-	res.status(200).json({ action: 'CREATE USER ROUTE' });
+const { User } = require('../../models');
+
+const postUser = async (req, res) => {
+	const { username, password } = req.body;
+
+	await User.create({
+		username: username,
+		password: password,
+	})
+		.then(() => {
+			res.status(200).json({ action: 'Created user.' });
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(404).json({ action: 'Could not create user.' });
+		});
 };
 
 module.exports = postUser;
